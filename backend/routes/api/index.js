d5b3054efa1015313fb3c5bf226a755e6342ae03
Emma Bross <emjbross@gmail.com>
2023-04-16 18:20:16 -0500

    Add a backend endpoint to get the current user session

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,8 +3,7 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
 
-// router.use(restoreUser);
-//if i get a bug comment this in and the other one out^
+router.use(restoreUser);
 
 // keep the restoreUser middleware connected before any other middleware or route handlers are connected to the router.This will allow all route handlers connected to this router to retrieve the current user on the Request object as req.user.If there is a valid current user session, then req.user will be set to the User in the database.If there is NO valid current user session, then req.user will be set to null.
 router.get(
@@ -14,7 +13,7 @@ router.get(
     }
 );
 
-router.use(restoreUser);
+// router.use(restoreUser);
 
 router.use('/session', sessionRouter);
 
