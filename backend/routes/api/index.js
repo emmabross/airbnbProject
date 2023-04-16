5be181849302a5dc3358bac4d13d15915716c6dd
Emma Bross <emjbross@gmail.com>
2023-04-16 15:31:07 -0500

    Add User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,34 @@
 const router = require('express').Router();
 
-router.post('/test', function (req, res) {
-    res.json({ requestBody: req.body });
-});
+// GET /api/restore-user
+const { restoreUser } = require('../../utils/auth.js');
+
+router.use(restoreUser);
+
+// keep the restoreUser middleware connected before any other middleware or route handlers are connected to the router.This will allow all route handlers connected to this router to retrieve the current user on the Request object as req.user.If there is a valid current user session, then req.user will be set to the User in the database.If there is NO valid current user session, then req.user will be set to null.
+router.get(
+    '/restore-user',
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
+
+//pretty sure this can be deleted but not 100% sure
+// router.post('/test', function (req, res) {
+//     res.json({ requestBody: req.body });
+// });
+
+//This will test the setTokenCookie function by getting the demo user and calling setTokenCookie.
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// router.get('/set-token-cookie', async (_req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'FakeJessie'
+//         }
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user: user });
+// });
 
 module.exports = router;
