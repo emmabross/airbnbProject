97956ed845e4e330caaff69ec97470e568a306ad
Emma Bross <emjbross@gmail.com>
2023-04-20 20:13:57 -0500

    Get all spots, still need to add avg rating to response

---
+++ backend/routes/api/spots.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const {Spot, User, SpotImage } = require('../../db/models');
+
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const router = express.Router();
+
+//Get all spots
+router.get("/", async (req, res) => {
+    const spots = await Spot.findAll({
+        include: [
+            {
+                model: SpotImage
+            }
+        ]
+    })
+
+    let spotsList = [];
+    spots.forEach(spot => {
+        spotsList.push(spot.toJSON())
+    });
+
+    spotsList.forEach(spot => {
+        spot.SpotImages.forEach(image => {
+            if (image.preview === true) {
+                spot.previewImage = image.url
+            }
+        })
+        if (!spot.previewImage) {
+            spot.previewImage = 'No spot image found'
+        }
+        delete spot.SpotImages;
+    })
+
+    res.json(spotsList)
+})
+
+module.exports = router;
